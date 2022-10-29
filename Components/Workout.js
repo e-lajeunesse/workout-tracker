e1c2565b52b3781070de62f62c25e495fd95f798
e-lajeunesse <eric.lajeunesse@outlook.com>
2022-10-29 11:27:42 -0500

    record workout

--- Components/Workout.js
+++ Components/Workout.js
@@ -10,13 +10,21 @@ export default function Workout() {
     const [title, setTitle] = useState("");
     const [newTitle, setNewTitle] = useState("");
     const [exercises, setExercises] = useState([]);
-    const [newExercise, setNewExercise] = useState({});
+    const [selectedExercise, setSelectedExercise] = useState({ value: "default", name: "" });
+    const [resistance, setResistance] = useState("");
+    const [sets, setSets] = useState("");
+
+
 
     useEffect(() => {
-        const saved = localStorage.getItem("exercises");
-        if (saved) {
+        const savedExercises = localStorage.getItem("exercises");
+        const savedTitle = localStorage.getItem("title");
+        if (savedExercises) {
 
-            setExercises(prevExercises => JSON.parse(saved));
+            setExercises(prevExercises => JSON.parse(savedExercises));
+        }
+        if (savedTitle) {
+            setTitle(savedTitle);
         }
     }, []);
 
@@ -25,6 +33,12 @@ export default function Workout() {
             localStorage.setItem("exercises", JSON.stringify(exercises));
         }
     }, [exercises])
+
+    useEffect(() => {
+        if (title.length > 0) {
+            localStorage.setItem("title", title);
+        }
+    }, [title])
 
     const handleTitleChange = (event) => {
         setNewTitle(event.target.value);
@@ -38,20 +52,15 @@ export default function Workout() {
 
     // see if better way to handle this, make a custom dropdown?
     const handleNameChange = (event) => {
-        if (event.target.value === "label") {
+        if (event.target.value === "default") {
             return;
         }
         const target = exercisesData.find(ex => ex.id == event.target.value);
-        setNewExercise(prevState => {
-            return { ...prevState, id: target.id, name: target.name }
-        });
+        setSelectedExercise({ value: target.id, name: target.name });
     }
 
     const handleResistanceChange = (event) => {
-        setNewExercise(prevState => {
-            return { ...prevState, resistance: event.target.value }
-        })
-
+        setResistance(event.target.value);
     }
 
     const handleSetsChange = (event) => {
@@ -59,15 +68,19 @@ export default function Workout() {
         for (let i = 1; i <= event.target.value; i++) {
             newSets.push({ number: i, reps: 0 })
         }
-        setNewExercise(prevState => {
-            return { ...prevState, sets: newSets }
-        })
+        setSets(newSets);
     }
 
     const newExerciseSubmit = (event) => {
-        setExercises(prevState => {
-            return [...prevState, newExercise]
+        if (selectedExercise.value === "default") {
+            return;
+        }
+        const resist = resistance === "" ? 0 : resistance;
+        const newExercise = { id: selectedExercise.value, name: selectedExercise.name, resistance: resist, sets: sets };
+        setExercises(prevExercises => {
+            return [...prevExercises, newExercise];
         })
+        setSelectedExercise({ value: "default", name: "" })
         event.preventDefault();
     }
 
@@ -78,7 +91,6 @@ export default function Workout() {
     const updateReps = (event, exerciseId, setNumber) => {
         let newReps = event.target.value;
         setExercises(prevState => {
-            console.log(event.target.value)
             return prevState.map(ex => {
                 if (ex.id === exerciseId) {
                     let updatedSets = ex.sets.map(s =>
@@ -91,16 +103,32 @@ export default function Workout() {
         })
     }
 
+    const recordWorkout = () => {
+        const finishedWorkout = {
+            date: new Date(),
+            title: title,
+            exercises: exercises
+        }
+        localStorage.clear();
+        localStorage.setItem("finishedWorkout", JSON.stringify(finishedWorkout));
+        setTitle("");
+        setNewTitle("");
+        setExercises([]);
+        setSelectedExercise({ vale: "default", name: "" });
+        setResistance("");
+        setSets("");
+    }
+
     return (
         <div className={styles.workout}>
-            <div>
-                {title.length > 0 ? 
-                    <div>
-                        <h2>{title}</h2>
-                        <FontAwesomeIcon icon={faPenToSquare} onClick={() => setTitle("")} />
-                    </div>:
-                    <form onSubmit={submitNewTitle}>
-                        <input type="text" placeholder="Enter Workout Title" onChange={handleTitleChange}></input>
+            <div className={styles.title}>
+                {title.length > 0 ?
+                    <div className={styles.titleEdit}>
+                        <h1>{title}</h1>
+                        <FontAwesomeIcon icon={faPenToSquare} className={styles.editIcon} onClick={() => setTitle("")} />
+                    </div> :
+                    <form className={styles.titleSet} onSubmit={submitNewTitle}>
+                        <input type="text" placeholder={newTitle.length > 0 ? newTitle : "Enter Workout Title"} onChange={handleTitleChange}></input>
                         <input type="submit" value="Set Workout Title" />
                     </form>}
             </div>
@@ -125,6 +153,7 @@ export default function Workout() {
                                             placeholder={`Set ${set.number} reps`}
                                             onChange={(event) => updateReps(event, ex.id, set.number)}
                                             name={ex.id}
+                                            value={set.reps > 0 ? set.reps : ""}
                                         >
                                         </input>
                                     </div>
@@ -136,21 +165,29 @@ export default function Workout() {
                 </tbody>
             </table>
 
-
-            <form onSubmit={newExerciseSubmit}>
-                <select onChange={handleNameChange}>
-                    <option value="label">Select Exercise</option>
-                    {exercisesData.map(ex => (
-                        <option key={ex.id} value={ex.id}> {ex.name} </option>
-                    ))}
-                </select>
-                <input type="number" placeholder="Enter Resistance" onChange={handleResistanceChange}></input>
-                <input type="number" placeholder="Enter Sets" onChange={handleSetsChange}></input>
-                <input type="submit" value="Add Exercise" />
-            </form>
-            <div className={styles.workoutButtons}>
-                <button> Record Workout </button>
+            <div className={styles.workoutAddRecord}>
+                <form className={styles.workoutAdd} onSubmit={newExerciseSubmit}>
+                    <select title="select exercise" onChange={handleNameChange} value={selectedExercise.value}>
+                        <option value="default" >Select Exercise</option>
+                        {exercisesData.map(ex => (
+                            <option
+                                key={ex.id}
+                                value={ex.id}
+                                disabled={exercises.map(e => e.id).includes(ex.id)}
+                            >
+                                {ex.name}
+                            </option>
+                        ))}
+                    </select>
+                    <input type="number" placeholder="Enter Resistance" onChange={handleResistanceChange}></input>
+                    <input type="number" placeholder="Enter Sets" onChange={handleSetsChange}></input>
+                    <input type="submit" value="Add Exercise" />
+                </form>
+                <div className={styles.workoutRecord}>
+                    <button onClick={recordWorkout}> Record Workout </button>
+                </div>
             </div>
+
         </div >
     )
 }
